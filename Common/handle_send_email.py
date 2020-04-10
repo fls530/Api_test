f614a9d23beac9ba905e27d4e5abc6cfa7a4f525
Administrator <530740300@qq.com>
2020-04-10 17:00:07 +0800

    Default Changelist

--- Common/handle_send_email.py
+++ Common/handle_send_email.py
@@ -1,35 +1,47 @@
+import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
+from Common.handle_path import REPORT_DIR
+from Common.handle_config import conf
+
 
 """
 qq邮箱：530740300@qq.com
 授权码：dtluwxbjpqrqbjba
 qq邮箱的smtp服务器地址：smtp.qq.com,端口：465
-
 """
-#1.连接smtp服务器,并登陆
-smtp = smtplib.SMTP_SSL(host="smtp.qq.com", port=465)
-smtp.login(user="530740300@qq.com", password="dtluwxbjpqrqbjba")
 
-#2.构造一封多组件邮件
-msg = MIMEMultipart()
-msg["Subject"] = "测试邮件01"
-msg["To"] = "lemonban@qq.com"
-msg["From"] = "530740300@qq.com"
 
-#构造邮件的文本内容
-text = MIMEText("邮件中的文本内容", _charset="utf8")
-msg.attach(text)
-# 构造邮件的附件
-with open(r"C:\Users\Administrator\Desktop\Api_test\Reports\report.html", "rb") as f:
-    content = f.read()
-report = MIMEApplication(content, _subtype='octet-stream')
-report.add_header('content-disposition', 'attachment', filename='python.html')
-msg.attach(report)
+class Send_email:
+    @staticmethod
+    def sendEmail():
+        # 1.连接smtp服务器,并登陆
+        smtp = smtplib.SMTP_SSL(conf.get("EMAIL", "host"), conf.get("EMAIL", "port"))
+        smtp.login(conf.get("EMAIL", "user"), conf.get("EMAIL", "password"))
 
-#3.发送邮件
-smtp.send_message(msg, from_addr="530740300@qq.com", to_addrs=["530740300@qq.com", "18562576336@163.com"])
+        # 2.构造一封多组件邮件
+        msg = MIMEMultipart()
+        msg["Subject"] = conf.get("EMAIL", "subject")
+        msg["To"] = conf.get("EMAIL", "To")
+        msg["From"] = conf.get("EMAIL", "From")
+
+        # 构造邮件的文本内容
+        text = MIMEText("邮件中的文本内容", _charset="utf8")
+        msg.attach(text)
+        # 构造邮件的附件
+        with open(os.path.join(REPORT_DIR, "report.html"), "rb") as f:
+            content = f.read()
+        report = MIMEApplication(content, _subtype='octet-stream')
+        report.add_header('content-disposition', 'attachment', filename='自动化测试报告.html')
+        msg.attach(report)
+
+        # 3.发送邮件
+        smtp.send_message(msg, from_addr=conf.get("EMAIL", "from_addr"), to_addrs=conf.get("EMAIL", "to_addrs"))
+
+
+
+
 
 
--- Run_test.py
+++ Run_test.py
@@ -2,6 +2,7 @@ import unittest
 from BeautifulReport import BeautifulReport
 from Common.handle_logging import log
 from Common.handle_path import CASE_DIR, REPORT_DIR
+from Common.handle_send_email import Send_email
 
 log.info("---------------开始执行测试用例-----------------------")
 
@@ -17,4 +18,6 @@ bf = BeautifulReport(suite)
 
 bf.report("注册接口", filename="report.html", report_dir=REPORT_DIR)
 
-log.info("---------------测试用例执行完毕-----------------")
+log.info("---------------测试用例执行完毕,正在发送邮件-----------------")
+
+Send_email.sendEmail()
